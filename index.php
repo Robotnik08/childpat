ba173573b19361fb3336f86b2661d696403ae226
Robotnik08 <89689979+Robotnik08@users.noreply.github.com>
2023-09-28 09:45:26 +0200

    index.php added

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>ChildPat</title>
+        <link rel="stylesheet" href="assets/css/main.css">
+        <script src="assets/js/main.js" type="module" defer></script>
+    </head>
+    <body>
+        
+    </body>
+</html>
