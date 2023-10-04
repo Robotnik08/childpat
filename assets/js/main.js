649081ead03a61bf19af2a179cf03b00649b0adc
Robotnik08 <89689979+Robotnik08@users.noreply.github.com>
2023-10-05 00:37:46 +0200

    Co-authored-by: Burrito-Princess <Burrito-Princess@users.noreply.github.com> Co-authored-by: Kajofzo <Kajofzo@users.noreply.github.com>

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,16 +5,33 @@ import { Sprite } from './modules/sprite.mjs';
 
 const canvas = new Canvas();
 const mainLoop = new Time();
+let angle = 0;
+let clawSpeed = 50;
+let boat_x = 400;
+
+const clawPos = new Vector2(100,100);
 mainLoop.start();
 
-const Rick = new Sprite('./assets/img/Rick.png');
+const claw_open = new Sprite('./assets/img/claw_open.png');
+const sea_03 = new Sprite('./assets/img/sea-03.png');
+const boat_01 = new Sprite('./assets/img/b1.png');
+// const sea_02 = new Sprite('./assets/img/sea-02.png');
 
 canvas.putCanvas();
 
 function draw () {
-    canvas.fitScreen();
+    canvas.smartFitScreen(4/3);
     canvas.clearCanvas();
-    canvas.drawSprite(Rick, new Vector2(100, 100), new Vector2(Rick.size.x, Rick.size.y));
+    canvas.drawSprite(sea_03, new Vector2(0, -650), new Vector2(1920, 1700));
+    canvas.drawSprite(boat_01, new Vector2(boat_x, 140));
+
+    // CLAW TEST
+    canvas.drawRotate(claw_open, clawPos, Math.sin(angle) * 50, new Vector2(100, 100));
+}
+
+function update (dt) {
+    angle += clawSpeed * dt;
 }
 
 mainLoop.subscribeUpdate(draw);
+mainLoop.subscribeUpdate(update);
--- assets/js/modules/canvas.mjs
+++ assets/js/modules/canvas.mjs
@@ -13,6 +13,7 @@ export class Canvas {
         });
         this.#canvas = document.createElement('canvas');
         this.#ctx = this.#canvas.getContext('2d');
+        this.ratio = 1 / 1;
     }
 
     fitScreen () { 
@@ -20,6 +21,19 @@ export class Canvas {
         this.#canvas.height = window.innerHeight;
     }
 
+    /**
+     * @brief Fits the canvas to the screen, but keeps the aspect ratio
+     */
+    smartFitScreen () {
+        const screenRatio = window.innerWidth / window.innerHeight;
+        if (screenRatio > this.ratio) {
+            this.#canvas.width = window.innerHeight * this.ratio;
+            this.#canvas.height = window.innerHeight;
+        } else {
+            this.#canvas.width = window.innerWidth;
+            this.#canvas.height = window.innerWidth / this.ratio;
+        }
+    }
     putCanvas () {
         document.body.appendChild(this.#canvas);
     }
@@ -32,13 +46,26 @@ export class Canvas {
     }
 
     drawRect (pos, size) {
+        pos = this.con(pos);
+        size = this.con(size);
         this.#ctx.fillRect(pos.x, pos.y, size.x, size.y);
     }
 
     drawCircle (pos, radius) {
+        pos = this.con(pos);
         this.#ctx.beginPath();
         this.#ctx.arc(pos.x, pos.y, radius, 0, 2 * Math.PI);
         this.#ctx.fill();
+    }
+
+    drawLine (pos1, pos2, thickness) {
+        pos1 = this.con(pos1);
+        pos2 = this.con(pos2);
+        this.#ctx.beginPath();
+        this.#ctx.moveTo(pos1.x, pos1.y);
+        this.#ctx.lineTo(pos2.x, pos2.y);
+        this.#ctx.lineWidth = thickness;
+        this.#ctx.stroke();
     }
 
     clearCanvas () {
@@ -46,17 +73,39 @@ export class Canvas {
     }
 
     drawSprite (sprite, pos, size = null) {
-        size ??= sprite.size;
-        console.log(sprite.isLoaded);
         if (!sprite.isLoaded) return;
+        size ??= sprite.size;
+        pos = this.con(pos);
+        size = this.con(size);
         this.#ctx.drawImage(sprite.img, pos.x, pos.y, size.x, size.y);
     }
 
-    drawRotate (sprite, pos, size, angle) {
-        
-    }
+    drawRotate (sprite, pos, angle, size = null) {
+        if (!sprite.isLoaded) return;
+        size ??= sprite.size;
+        pos = this.con(pos);
+        size = this.con(size);
+        this.#ctx.save();
+        this.#ctx.translate(pos.x + size.x / 2, pos.y + size.y / 2);
+        this.#ctx.rotate(angle * Math.PI / 180);
+        this.#ctx.drawImage(sprite.img, pos.x, pos.y, size.x, -size.y);
+        this.#ctx.restore();
 
-    rotateCanvas (angle) {
-        this.#ctx.rotate(angle);
+    }
+    /**
+     * @brief Converts a vector corresponding to the canvas size
+     * @param {Vector2} vec2 
+     * @returns 
+     */
+    con (vec2) {
+        const screenRatio = window.innerWidth / window.innerHeight;
+        const canvasRatio = this.#canvas.width / this.#canvas.height;
+        if (screenRatio > canvasRatio) {
+            const scale = this.#canvas.height / window.innerHeight;
+            return new Vector2(vec2.x * scale, vec2.y * scale);
+        } else {
+            const scale = this.#canvas.width / window.innerWidth;
+            return new Vector2(vec2.x * scale, vec2.y * scale);
+        }
     }
 }
